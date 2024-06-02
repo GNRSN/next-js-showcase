f6ed51e02ae0e9266b46669e59d61b8ef40a0cb4
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-06-02 10:11:43 +0200

    add calendar headers + align to ISO weekdays

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,4 +1,9 @@
+import { cn } from "@/lib/utils";
 import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+import localeData from "dayjs/plugin/localeData";
+
+dayjs.extend(isoWeek);
 
 /**
  *  Idea
@@ -15,9 +20,9 @@ export default function CalendarPage() {
   const daysInMonth = [];
 
   // Pad start with end of previous month
-  if (firstInMonth.day() > 0) {
-    for (let i = 0; i < firstInMonth.day(); i++) {
-      daysInMonth.push(firstInMonth.day(i));
+  if (firstInMonth.isoWeekday() > 0) {
+    for (let i = 1; i < firstInMonth.isoWeekday(); i++) {
+      daysInMonth.push(firstInMonth.isoWeekday(i));
     }
   }
 
@@ -27,23 +32,42 @@ export default function CalendarPage() {
   }
 
   // Pad end with start of next month
-  if (lastInMonth.day() < 6) {
-    for (let i = 0; i < 6 - lastInMonth.day(); i++) {
-      daysInMonth.push(lastInMonth.day(i));
+  if (lastInMonth.isoWeekday() < 7) {
+    for (let i = lastInMonth.isoWeekday() + 1; i <= 7; i++) {
+      daysInMonth.push(lastInMonth.isoWeekday(i));
     }
   }
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="text-2xl font-bold">{now.format("MMMM")}</div>
         <div className="grid grid-cols-7">
-          {daysInMonth.map((d) => (
-            <div
-              className={`col-start-${d.day() + 1} col-end-${d.day() + 2} p-4`}
-            >
-              {d.format("YYYY-MM-DD")}
-            </div>
-          ))}
+          {daysInMonth.slice(0, 7).map((d) => {
+            return (
+              <div className="text-gray-500 p-4 font-bold">
+                {d.format("ddd")}
+              </div>
+            );
+          })}
+          {daysInMonth.map((d) => {
+            const isDateInOtherMonth = d.month() !== now.month();
+            const isToday = d.isSame(now, "date");
+
+            return (
+              <div
+                className={cn(
+                  `col-start-${d.isoWeekday()} col-end-${d.isoWeekday() + 1} p-4`,
+                  {
+                    "opacity-50": isDateInOtherMonth,
+                    "text-blue-500": isToday,
+                  },
+                )}
+              >
+                {d.format("YYYY-MM-DD")}
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
